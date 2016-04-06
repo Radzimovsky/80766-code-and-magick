884859672ac3d52aff9da1023524f6c9c8201d5b
Radzimovsky <aleradzimovsk@gmail.com>
2016-04-06 17:02:25 +0300

    Текстовое поле и перенос по строкам.

--- build/js/check.js
+++ build/js/check.js
@@ -31,3 +31,48 @@ function getMessage(a, b) {
     }
   }
 }
+
+function getMessage(a, b) {
+  if(typeof(a) == 'boolean'){
+    return a ? 'Я попал в ' + b : 'Я никуда не попал';
+    }
+  }
+
+
+
+  else{
+    if(Array.isArray(a) && Array.isArray(b)){
+      var length = 0;
+      a.forEach(function(item, index){
+        length += (item * b[index]);
+      });
+      return 'Я прошёл ' + length + ' метров';
+    }
+    else {
+      if(Array.isArray(a)){
+        var sum = 0;
+        a.forEach(function(item, index){
+          sum += item
+        });
+        return 'Я прошёл ' + sum +' шагов';
+      }
+    }
+
+  if(type (a) == 'object'){
+    var sum = 0;
+    var i = 0;
+    while (i < a.length) {
+      sum += a[i];
+      ++i;
+    }
+    return 'Я прошёл ' + sum +' шагов';
+  }
+
+
+    if(type (a) == 'object' && type (b) == 'object') {
+      var length = 0;
+      for (i=0; i<a.length; ++i){
+        length += a[i] * b[i];
+      }
+        return 'Я прошёл ' + length + ' метров';
+    }
