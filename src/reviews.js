8972b3806cbcbace85d7347f665a3fa0b771096a
Radzimovsky <aleradzimovsk@gmail.com>
2016-04-20 17:29:05 +0300

    промежуточный

--- src/reviews.js
+++ src/reviews.js
@@ -6,6 +6,7 @@
   var templateElement = document.querySelector('#review-template');
   var elementToClone;
   var IMAGE_LOAD_TIMEOUT = 10000;
+  var filtersContainer = document.querySelector('.reviews-filter');
 
   filters.classList.add('invisible');
 
@@ -38,8 +39,72 @@
     }, IMAGE_LOAD_TIMEOUT);
     return element;
   }
-  window.reviews.forEach(function(review) {
-    createReviewElement(review, reviewContainer);
+
+  var REVIEWS_LOAD_URL = '//o0.github.io/assets/json/reviews.json';
+
+  var getReviews = function(callback) {
+    var xhr = new XMLHttpRequest();
+
+    xhr.onload = function(evt) {
+      var loadedData = JSON.parse(evt.target.response);
+      callback(loadedData);
+    };
+
+    xhr.open('GET', REVIEWS_LOAD_URL);
+    xhr.send();
+  };
+
+  var renderReviews = function(reviews) {
+    reviews.forEach(function(review) {
+      createReviewElement(review, reviewContainer);
+    });
+  };
+
+  var setFiltrationEnabled = function() {
+    var filters = filtersContainer.querySelectorAll('input[name=reviews]');
+    for (var i = 0; i < filters.length; i++) {
+      filters[i].onchange = function(evt) {
+        setFilterEnabled(this.id);
+      };
+    }
+  };
+
+  var setFilterEnabled = function(filter) {
+    var filteredReviews = getFilteredReviews(reviews, filter);
+    renderReviews(filteredReviews);
+  };
+
+  var getFilteredReviews = function(reviews, filter) {
+    var reviewsToFilter = reviews.slice(0);
+
+    switch (filter) {
+      case 'reviews-recent':
+        reviewsToFilter.sort(function(a, b) {
+          return a.price - b.price;
+        });
+        break;
+      case 'reviews-good':
+        reviewsToFilter.filter(function(a) {
+          return a.rating >= 3;
+        });
+        break;
+      case 'reviews-bad':
+        reviewsToFilter = [];
+        break;
+      case 'reviews-popular':
+        break;
+      case 'default':
+        reviewsToFilter = reviews;
+        break;
+    }
+
+    return reviewsToFilter;
+  };
+
+  getReviews(function(loadedReviews) {
+    reviews = loadedReviews;
+    setFiltrationEnabled();
+    renderReviews(reviews);
   });
 
   filters.classList.remove('invisible');
